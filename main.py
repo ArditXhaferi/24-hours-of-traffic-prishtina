e9063ec66b39ae462eee4d29ca5de74f06c6cf4d
arditxh0 <ardit@starlabspro.com>
2022-11-01 11:23:49 +0100

    Headless

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 from datetime import datetime
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--window-size=1600, 900')  # Last I checked this was necessary.
 
 now = datetime.now()
 dt_string = now.strftime("%d-%m-%Y %H:%M")
@@ -18,18 +24,18 @@ def delete(path):
     time.sleep(.1)
 
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
  
 
-driver.get('https://www.google.com/maps/@42.6539778,21.1589676,15z/data=!5m1!1e1')
+driver.get('https://www.google.com/maps/@42.6584911,21.1631733,15z/data=!5m1!1e1')
 
 time.sleep(1)
 
-element = driver.find_element_by_xpath("/html/body/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button")
+# element = driver.find_element_by_xpath("/html/body/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button")
 
-element.click()
+# element.click()
 
-time.sleep(4)
+# time.sleep(4)
 
 delete("/html/body/div[3]/div/div[26]")
 delete("/html/body/div[3]/div[9]/div[23]/div[1]/div[2]")
