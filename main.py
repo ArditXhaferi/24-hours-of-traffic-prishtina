5e64abc0ca12ff84002b7657fa13b15a1a597591
arditxh0 <ardit@starlabspro.com>
2022-11-12 23:38:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,28 +4,30 @@ from selenium.webdriver.common.by import By
 import time
 from datetime import datetime
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 options = Options()
-options.add_argument("start-maximized")
 options.add_argument('--no-sandbox')
-options.add_argument("--disable-setuid-sandbox")
+options.add_argument('--headless')
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("enable-automation")
-options.add_argument("--window-size=1920,1080")
-options.add_argument("--disable-extensions")
-options.add_argument("--dns-prefetch-disable")
 options.add_argument('--window-size=900, 900')  # Last I checked this was necessary.
-caps = DesiredCapabilities().CHROME
-caps["pageLoadStrategy"] = "normal"  #  complete
+
 now = datetime.now()
 dt_string = now.strftime("%d-%m-%Y %H:%M")
 
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=options, desired_capabilities=caps)
-driver = webdriver.Chrome(executable_path="/var/www/Google-Maps-Clean-Screenshot/chrome", chrome_options=options)
+def delete(path):
+    element = driver.find_element(By.XPATH, path)
+
+    driver.execute_script("""
+    var element = arguments[0];
+    element.parentNode.removeChild(element);
+    """, element)
+
+    time.sleep(.1)
+
+
+# driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=options)
 
 print('test')
 driver.get('http://127.0.0.1:8080/index.html')
@@ -35,7 +37,7 @@ print('tes2')
 
 # element.click()
 
-time.sleep(10)
+time.sleep(2)
 
 
 driver.save_screenshot(f'./images/{dt_string}.png')
