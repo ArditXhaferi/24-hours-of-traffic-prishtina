b7c177b145b9765d79d5b005fb6fa7aedbaeae20
arditxh0 <ardit@starlabspro.com>
2022-11-13 00:08:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -52,12 +52,16 @@
 # driver.quit()
 
 import requests
+import urllib.parse
 
-BASE = 'https://render-tron.appspot.com/screenshot/'
-url = 'https://google.com'
-path = 'target.jpg'
+BASE = 'https://mini.s-shot.ru/1024x0/JPEG/1024/Z100/?' # you can modify size, format, zoom
+url = 'https://stackoverflow.com/'#or whatever link you need
+url = urllib.parse.quote_plus(url) #service needs link to be joined in encoded format
+print(url)
+
+path = 'target1.jpg'
 response = requests.get(BASE + url, stream=True)
-# save file, see https://stackoverflow.com/a/13137873/7665691
+
 if response.status_code == 200:
     with open(path, 'wb') as file:
         for chunk in response:
