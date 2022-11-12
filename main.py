0b086dbf95468919be8d87de3a576cfc8d69d7a9
arditxh0 <ardit@starlabspro.com>
2022-11-13 00:00:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,64 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-import time
-from datetime import datetime
-from selenium.webdriver.chrome.options import Options
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.common.by import By
+# import time
+# from datetime import datetime
+# from selenium.webdriver.chrome.options import Options
 
-options = Options()
-# options.add_argument('--no-sandbox')
-# options.add_argument('--headless')
-# options.add_argument('--headless')
-# options.add_argument('--disable-gpu')
-# options.add_argument('--window-size=900, 900')  # Last I checked this was necessary.
+# options = Options()
+# # options.add_argument('--no-sandbox')
+# # options.add_argument('--headless')
+# # options.add_argument('--headless')
+# # options.add_argument('--disable-gpu')
+# # options.add_argument('--window-size=900, 900')  # Last I checked this was necessary.
+# # options.add_argument("--disable-dev-shm-usage")
+# options.add_argument("start-maximized")
+# options.add_argument("disable-infobars")
+# options.add_argument("--disable-extensions")
+# options.add_argument("--disable-gpu")
 # options.add_argument("--disable-dev-shm-usage")
-options.add_argument("start-maximized")
-options.add_argument("disable-infobars")
-options.add_argument("--disable-extensions")
-options.add_argument("--disable-gpu")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--no-sandbox")
+# options.add_argument("--no-sandbox")
 
-now = datetime.now()
-dt_string = now.strftime("%d-%m-%Y %H:%M")
+# now = datetime.now()
+# dt_string = now.strftime("%d-%m-%Y %H:%M")
 
-def delete(path):
-    element = driver.find_element(By.XPATH, path)
+# def delete(path):
+#     element = driver.find_element(By.XPATH, path)
 
-    driver.execute_script("""
-    var element = arguments[0];
-    element.parentNode.removeChild(element);
-    """, element)
+#     driver.execute_script("""
+#     var element = arguments[0];
+#     element.parentNode.removeChild(element);
+#     """, element)
 
-    time.sleep(.1)
+#     time.sleep(.1)
 
-time.sleep(5)
-# driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
-driver = webdriver.Chrome("/snap/bin/chromium.chromedriver", chrome_options=options)
+# time.sleep(5)
+# # driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+# driver = webdriver.Chrome("/snap/bin/chromium.chromedriver", chrome_options=options)
 
-print('test')
-driver.get('http://127.0.0.1:8080/index.html')
-print('tes2')
+# print('test')
+# driver.get('http://127.0.0.1:8080/index.html')
+# print('tes2')
 
-# element = driver.find_element_by_xpath("/html/body/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button")
+# # element = driver.find_element_by_xpath("/html/body/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button")
 
-# element.click()
+# # element.click()
 
-time.sleep(2)
+# time.sleep(2)
 
 
-driver.save_screenshot(f'./images/{dt_string}.png')
+# driver.save_screenshot(f'./images/{dt_string}.png')
  
-driver.quit()
+# driver.quit()
+
+import requests
+
+BASE = 'https://render-tron.appspot.com/screenshot/'
+url = 'https://google.com'
+path = 'target.jpg'
+response = requests.get(BASE + url, stream=True)
+# save file, see https://stackoverflow.com/a/13137873/7665691
+if response.status_code == 200:
+    with open(path, 'wb') as file:
+        for chunk in response:
+            file.write(chunk)
