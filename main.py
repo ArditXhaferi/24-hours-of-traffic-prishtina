69254ac7412bedde727ee303b14a933cd2addd70
arditxh0 <ardit@starlabspro.com>
2022-11-12 19:35:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,6 @@ driver.get('http://127.0.0.1:5500/index.html')
 time.sleep(2)
 
 
-driver.save_screenshot(f'/Users/arditxhaferi/Documents/Projects/Google-Maps-Clean-Screenshot/images/{dt_string}.png')
+driver.save_screenshot(f'./images/{dt_string}.png')
  
 driver.quit()
