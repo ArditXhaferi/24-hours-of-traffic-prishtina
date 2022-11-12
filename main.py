5f42650c6cbeb84f6817732896fe3a2b727718c8
arditxh0 <ardit@starlabspro.com>
2022-11-12 23:26:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,22 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 options = Options()
-options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument('--no-sandbox')
 options.add_argument("--disable-setuid-sandbox")
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument("--disable-dev-shm-usage");
+options.add_argument("enable-automation")
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--disable-extensions")
+options.add_argument("--dns-prefetch-disable")
+options.add_argument('--window-size=900, 900')  # Last I checked this was necessary.
+caps = DesiredCapabilities().CHROME
+caps["pageLoadStrategy"] = "normal"  #  complete
 now = datetime.now()
 dt_string = now.strftime("%d-%m-%Y %H:%M")
 
-time.sleep(5)
 # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=options, desired_capabilities=caps)
 driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=options)
 
