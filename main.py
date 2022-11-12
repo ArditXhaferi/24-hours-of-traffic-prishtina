5d81e478be6755237edb7bea82cb5f2f88b4fe45
arditxh0 <ardit@starlabspro.com>
2022-11-12 23:55:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ from datetime import datetime
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
-options.add_argument('--no-sandbox')
-options.add_argument('--headless')
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--window-size=900, 900')  # Last I checked this was necessary.
+# options.add_argument('--no-sandbox')
+# options.add_argument('--headless')
+# options.add_argument('--headless')
+# options.add_argument('--disable-gpu')
+# options.add_argument('--window-size=900, 900')  # Last I checked this was necessary.
+# options.add_argument("--disable-dev-shm-usage")
+options.add_argument("start-maximized")
+options.add_argument("disable-infobars")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-gpu")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
 
 now = datetime.now()
 dt_string = now.strftime("%d-%m-%Y %H:%M")
