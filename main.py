a797a75d86920dee2fa93b89f70dbdd242472ac4
arditxh0 <ardit@starlabspro.com>
2022-11-12 22:35:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_argument('--no-sandbox')
+options.add_argument("--disable-setuid-sandbox")
 options.add_argument('--headless')
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
